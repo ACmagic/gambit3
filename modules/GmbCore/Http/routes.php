8a153fd9ce338fde54f8d474f13043fd6cb9e1ac
tzmijewski <sme.s.key@gmail.com>
2016-02-22 20:14:37 -0500

    Remove GmcCore module.

--- modules/GmbCore/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'gmbcore', 'namespace' => 'Modules\GmbCore\Http\Controllers'], function()
-{
-	Route::get('/', 'GmbCoreController@index');
-});
