cfbde96af91c1ba55648aaa53b03f4e465d65eed
tzmijewski <sme.s.key@gmail.com>
2016-03-07 19:24:04 -0500

    Add machine_name column to stores and sites.

---
+++ database/migrations/Version20160308000555.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema as Schema;
+use LaravelDoctrine\Migrations\Schema\Table;
+use LaravelDoctrine\Migrations\Schema\Builder;
+
+class Version20160308000555 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+
+        (new Builder($schema))->table('stores', function(Table $table) {
+            $table->string('machine_name',128)->setNotNull(true)->setDefault('');
+            $table->unique('machine_name');
+        });
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+
+        (new Builder($schema))->table('stores', function(Table $table) {
+            $table->dropColumn('machine_name');
+        });
+
+    }
+}
